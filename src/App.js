0b700267104fa01925bb90492622660f754de42e
Leon Gaban <leongaban@gmail.com>
2024-02-11 21:01:45 -0600

    converted App to a functional component

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
 import Scroll from './Scroll'
-// import { robots as robotsArray } from './robots'
 import './App.css'
 
-class App extends Component {
-  constructor() {
-    super()
+const App = () => {
+  const [searchField, setSearchField] = useState('')
+  const [robots, setRobots] = useState([])
 
-    this.state = {
-      robots: [],
-      searchfield: '',
-    }
-  }
-
-  // prettier-ignore
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
-        .then((res) => res.json())
-        .then(users => this.setState({ robots: users }))
-  }
+      .then(res => res.json())
+      .then(users => setRobots(users))
+  }, [])
 
-  onSearchChange = (event) => {
-    const searchValue = event.target.value.toLowerCase()
+  const onSearchChange = event => setSearchField(event.target.value)
 
-    this.setState({
-      searchfield: searchValue,
-    })
-  }
+  const filteredRobots = robots.filter(robot =>
+    robot.name.toLowerCase().includes(searchField.toLowerCase())
+  )
 
-  render() {
-    const { robots, searchfield } = this.state
-
-    const filteredRobots = robots.filter((robot) => {
-      return robot.name.toLowerCase().includes(searchfield)
-    })
-
-    return !robots.length ? (
-      <h1>Loading</h1>
-    ) : (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList robots={filteredRobots} />
-        </Scroll>
-      </div>
-    )
-  }
+  return !robots.length ? (
+    <h1>Loading</h1>
+  ) : (
+    <div className="tc">
+      <h1 className="f1">RoboFriends</h1>
+      <SearchBox searchfield={searchField} searchChange={onSearchChange} />
+      <Scroll>
+        <CardList robots={filteredRobots} />
+      </Scroll>
+    </div>
+  )
 }
 
 export default App
